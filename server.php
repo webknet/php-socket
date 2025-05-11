5d1125403c3ec16c1f180fd025f7cc4ad195e9c1
Sabino Teixeira <webknet@gmail.com>
2025-05-11 17:24:45 +0000

    Update server.php

--- server.php
+++ server.php
@@ -1,1 +1,19 @@
+<?php
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+use MyApp\TodoServer;
 
+require dirname(__DIR__) . '/html/vendor/autoload.php';
+
+// Servidor WebSocket
+$wsServer = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new TodoServer()
+        )
+    ),
+    8080
+);
+
+$wsServer->run();
