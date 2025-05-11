03b94c6978e2f6a1e30739de0e85bc9d06d064bd
Sabino Teixeira <webknet@gmail.com>
2025-05-11 17:28:27 +0000

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,1 +1,25 @@
+<?php
+class Database {
+    private $pdo;
 
+    public function __construct() {
+        $host = '127.0.0.1';
+        $dbname = 'todo_app';
+        $port = '3306';
+        $user = 'root'; // Altere para seu usuário
+        $pass = '1234';     // Altere para sua senha
+
+        try {
+            $this->pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erro de conexão: " . $e->getMessage());
+        }
+    }
+
+    public function getPdo() {
+        return $this->pdo;
+    }
+}
+?>
